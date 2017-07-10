a094a68eca87e2085b828829f5a40abbac1c65b2
Vibert Thio <vibertthio@gmail.com>
2017-07-11 02:29:01 +0800

    first commit

---
+++ server/api/models/Drum.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+
+const DrumSchema = new Schema({
+  id: String,
+  title: String,
+  content: Array,
+  startTime: Number,
+});
+
+const Drum = mongoose.model('notes', DrumSchema);
+const DrumPattern = mongoose.model('patterns', DrumSchema);
+
+module.exports = { Drum, DrumPattern };
